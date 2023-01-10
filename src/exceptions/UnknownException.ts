26b9716130a3f43e141951f35de4cb2e800ff9f6
Victor Godinho <dragonsgamers@hotmail.com.br>
2023-01-10 19:45:03 -0300

    feat(Exception): :sparkles: add parse exception default value

--- src/exceptions/UnknownException.ts
+++ src/exceptions/UnknownException.ts
@@ -2,4 +2,16 @@ import { Exception } from "./Exception";
 
 export class UnknownException extends Exception {
 
+    /**
+     * Parse error to Exception
+     *
+     * @param {Error | Exception | unknown} exception Possible exception
+     * @param {string} message Message Default Exception
+     * @returns {Exception | UnknownException}
+     * @memberof Exception
+     */
+    public static parseOrDefault(exception: unknown, message: string): Exception | UnknownException {
+        return this.parse(exception) ?? new this(message, exception);
+    }
+
 }
---
+++ tests/jest/parseOrDefault.test.ts
@@ -0,0 +1,17 @@
+import { UnknownException } from "../../src";
+
+describe("Test Default Exception", () => {
+    test("Test UnknownException", async () => {
+        const message = "test default";
+        await expect((async (): Promise<never> => {
+            throw UnknownException.parseOrDefault(undefined, message);
+        })()).rejects.toThrowError(new UnknownException(message));
+    });
+
+    test("Test Error", async () => {
+        const message = "ok";
+        await expect((async (): Promise<never> => {
+            throw UnknownException.parseOrDefault(new Error(message), "anything");
+        })()).rejects.toThrowError(new UnknownException(message));
+    });
+});
